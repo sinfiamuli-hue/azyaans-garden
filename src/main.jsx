6b4760f7fac7f559a82adffa0005bce80f158985
sinfiamuli-hue <sinfiamuli@gmail.com>
2026-09-18 22:39:08 +0500

    Refactor main.jsx to include routing and context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,16 @@
-import React from'react';import{createRoot}from'react-dom/client';import App from'./App';import'./index.css';createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { BasketProvider } from "./context/BasketContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <BasketProvider>
+        <App />
+      </BasketProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
