1c583dfd3f7c1a35f0a2d796c888b71d2f4149ec
sinfiamuli-hue <sinfiamuli@gmail.com>
2026-09-18 20:30:40 +0500

    Add files via upload

---
+++ pages/PolicyPage.jsx
@@ -0,0 +1,1 @@
+import{SeoMeta}from'../components/SeoMeta';import{CollectionPolicy}from'../components/CollectionPolicy';export function PolicyPage(){return <><SeoMeta title="Collection & Delivery"/><CollectionPolicy/></>}
