9536e2913209fb0bddc2bd01f65b3f86fad82250
sinfiamuli-hue <sinfiamuli@gmail.com>
2026-09-18 22:07:31 +0500

    Update currency.js

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -1,1 +1,8 @@
+import siteConfig from "../config/site.js";
 
+// formatPrice(120) -> "MVR 120"
+export function formatPrice(amount) {
+  const rounded =
+    Number.isInteger(amount) ? amount : Math.round(amount * 100) / 100;
+  return `${siteConfig.currency} ${rounded.toLocaleString("en-US")}`;
+}
